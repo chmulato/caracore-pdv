3e1f6e46005cfdbb2616b07187bcdb088e19f20e
chmulato <chmulato@gmail.com>
2017-02-14 14:40:45 -0200

    novo parâmetro para filtro de produto da tela pdv.

--- src/main/java/br/com/caracore/pdv/repository/filter/ProdutoFilter.java
+++ src/main/java/br/com/caracore/pdv/repository/filter/ProdutoFilter.java
@@ -2,7 +2,17 @@ package br.com.caracore.pdv.repository.filter;
 
 public class ProdutoFilter {
 	
+	private String codigo;
+	
 	private String descricao;
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
 
 	public String getDescricao() {
 		return descricao;
