bb4488f017241c67bf43664313bee12561c8ef96
chmulato <chmulato@gmail.com>
2017-03-10 09:56:31 -0300

    Ajustar arquivo javascript para página própria.

---
+++ src/main/resources/static/layout/javascripts/forma-pagamento.js
@@ -0,0 +1,10 @@
+$("#pesquisa_cliente").easyAutocomplete(options);
+$(document).on("change", ".js-somar-valor", function() {
+    var sum = 0;
+    $(".js-somar-valor").each(function() {
+    	sum += +$(this).val().replace('.','').replace(',','.');
+    });
+    sum = parseFloat(Math.round(sum * 100) / 100).toFixed(2);
+    sum = sum.replace('.',',');
+    $(".js-total-da-soma").val(sum);
+});
