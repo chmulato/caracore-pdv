bdfe1f4b7c56cbf57e09d5766e27d6ffc6685862
Christian Mulato <chmulato@hotmail.com>
2020-03-26 09:03:46 -0300

    Configurando para deploy no tomcat.

---
+++ src/main/java/br/com/caracore/pdv/SpringBootWarDeploymentApplication.java
@@ -0,0 +1,19 @@
+package br.com.caracore.pdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootWarDeploymentApplication  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWarDeploymentApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWarDeploymentApplication.class, args);
+    }
+}
